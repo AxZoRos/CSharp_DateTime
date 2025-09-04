02c8007c708272e67ca43127770b5b68a89640c1
AxZoRos <zenjakonovalov154@gmail.com>
2025-09-05 02:07:46 +0300

    add project
    Додано проєкт

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB10_KTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            DateTime currentDate = DateTime.Now; //отримує поточну дату та час
+            Console.WriteLine("Поточна дата та час: " + currentDate);
+            Console.Write("Введіть кількість годин, які потрібно додати: ");
+            string inputHours = Console.ReadLine();
+            int additionalHours = Convert.ToInt32(inputHours);
+            DateTime futureDate = currentDate.AddHours(additionalHours);// Кількість годин додається до поточної дати
+            Console.WriteLine("Дата і час через " + additionalHours + " годин(и): " + futureDate);
+            Console.ReadLine();
+        }
+    }
+}
